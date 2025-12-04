1790075915399f7d972132633ebd49a5afe078f5
Benno Weber <bennopaul.weber@gmail.com>
2025-12-04 16:35:18 +0100

    minor changes

--- Bots/BaseChessBot.py
+++ Bots/BaseChessBot.py
@@ -30,16 +30,9 @@ def chess_bot(player_sequence, board, time_budget, **kwargs):
     ev = evaluate(board,color,piece_values)
     print("evaluation : " + str(ev))
 
-    for x in range(board.shape[0]-1):
-        for y in range(board.shape[1]):
-            if board[x,y] != "p"+color:
-                continue
-            if y > 0 and board[x+1,y-1] != '' and board[x+1,y-1][-1] != color:
-                return (x,y), (x+1,y-1)
-            if y < board.shape[1] - 1 and board[x+1,y+1] != '' and board[x+1,y+1][1] != color:
-                return (x,y), (x+1,y+1)
-            elif board[x+1,y] == '':
-                return (x,y), (x+1,y)
+    nextboards = []
+    
+
 
     return (0,0), (0,0)
 
