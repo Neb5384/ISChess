4a84c46fa90f298de4f3e5c376769d3fa5a5e197
Dyumes <76482455+Dyumes@users.noreply.github.com>
2025-12-04 16:33:10 +0100

    Pawn movement added + moves list

--- Bots/BaseChessBot.py
+++ Bots/BaseChessBot.py
@@ -12,20 +12,34 @@ from Bots.ChessBotList import register_chess_bot
 
 #   Simply move the pawns forward and tries to capture as soon as possible
 def chess_bot(player_sequence, board, time_budget, **kwargs):
+    pieces = ['p', 'n', 'b', 'r', 'q', 'K']
+    
+    all_moves=[]
 
     color = player_sequence[1]
-    for x in range(board.shape[0]-1):
+    for x in range(board.shape[0]):
         for y in range(board.shape[1]):
-            if board[x,y] != "p"+color:
-                continue
-            if y > 0 and board[x+1,y-1] != '' and board[x+1,y-1][-1] != color:
-                return (x,y), (x+1,y-1)
-            if y < board.shape[1] - 1 and board[x+1,y+1] != '' and board[x+1,y+1][1] != color:
-                return (x,y), (x+1,y+1)
-            elif board[x+1,y] == '':
-                return (x,y), (x+1,y)
-
+            if board[x,y] != "":
+                piece = board[x,y]
+                if piece.color + piece.type == color + 'p' :
+                    print("Call move Pawn")
+                    all_moves.append(((x,y),(movePawn(board, x, y).copy())))
+                    print("All pawn moves" + str(all_moves))
     return (0,0), (0,0)
+    
 
+    
 #   Example how to register the function
-register_chess_bot("PawnMover", chess_bot)
+
+def movePawn(board, x, y):
+    moveList = []
+    if board[x+1,y] == "":
+        moveList.append((x+1, y))
+    if y+1 <= 7 and (board[x+1,y+1] != "" and board[x+1,y+1].color != color):
+        moveList.append((x+1, y+1))
+    if y-1 >= 0 and (board[x+1,y-1] != "" and board[x+1,y-1].color != color):
+        moveList.append((x+1, y-1))
+    return moveList
+
+
+register_chess_bot("A.L.P.H.A", chess_bot)
