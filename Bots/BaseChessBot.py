c3f3342a24a579aada4f883229cf7feef99ccce1
Benno Weber <bennopaul.weber@gmail.com>
2025-12-04 15:58:34 +0100

    added basic evaluation function

--- Bots/BaseChessBot.py
+++ Bots/BaseChessBot.py
@@ -12,8 +12,24 @@ from Bots.ChessBotList import register_chess_bot
 
 #   Simply move the pawns forward and tries to capture as soon as possible
 def chess_bot(player_sequence, board, time_budget, **kwargs):
-
     color = player_sequence[1]
+    piece_values = {
+        "wp" : 1,
+        "bp" : -1,
+        "wn" : 3,
+        "bn" : -3,
+        "wb" : 3,
+        "bb" : -3,
+        "wr" : 5,
+        "br" : -5,
+        "wq" : 9,
+        "bq" : -9,
+        "wk" : 0,
+        "bk" : 0
+        }
+    ev = evaluate(board,color,piece_values)
+    print("evaluation : " + str(ev))
+
     for x in range(board.shape[0]-1):
         for y in range(board.shape[1]):
             if board[x,y] != "p"+color:
@@ -26,6 +42,18 @@ def chess_bot(player_sequence, board, time_budget, **kwargs):
                 return (x,y), (x+1,y)
 
     return (0,0), (0,0)
+
+
+def evaluate(board,color,piece_values):
+    score = 0
+    for x in range(board.shape[0]):
+        for y in range(board.shape[1]):
+            if board[x,y] != "":
+                piece = board[x,y]
+                score += piece_values[piece.color + piece.type]
+                #print(piece.type + piece.color + str(score))
+    if color == 'b': score = -score 
+    return score
 
 #   Example how to register the function
 register_chess_bot("PawnMover", chess_bot)
